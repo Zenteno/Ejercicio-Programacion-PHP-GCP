97c68a0842f78f3a00163621acb8022694b4a56a
Alberto Zenteno <x.zenteno.a@gmail.com>
2020-06-04 19:22:48 -0400

    modify routes middleware

--- routes/web.php
+++ routes/web.php
@@ -14,27 +14,22 @@
 $router->get('/token', 'TokenController@tokenizer');
 
 $router->group(['prefix' => 'courses', "middleware"=>"jwt"], function () use ($router) {
-    $router->get('', 'CourseController@indexPaginated');
-    $router->get('all', 'CourseController@index');
-    $router->get('{id}', 'CourseController@show');
-    $router->delete('{id}', 'CourseController@destroy');
-    
     $router->group(['middleware'=>'course'],function () use ($router){
+        $router->get('', 'CourseController@indexPaginated');
+        $router->get('all', 'CourseController@index');
+        $router->get('{id}', 'CourseController@show');
+        $router->delete('{id}', 'CourseController@destroy');
         $router->post('','CourseController@store');
         $router->put('{id}','CourseController@update');        
     });
     
-});
-
-$router->group(['prefix' => 'students', "middleware"=>"jwt"], function () use ($router) {
-    $router->get('', 'StudentController@indexPaginated');
-    $router->get('all', 'StudentController@index');
-    $router->get('{id}', 'StudentController@show');
-    $router->delete('{id}', 'StudentController@destroy');
-
     $router->group(['middleware'=>'student'],function () use ($router){
         $router->post('','StudentController@store');
         $router->put('{id}','StudentController@update');        
+        $router->get('', 'StudentController@indexPaginated');
+        $router->get('all', 'StudentController@index');
+        $router->get('{id}', 'StudentController@show');
+        $router->delete('{id}', 'StudentController@destroy');
     });
-    
+
 });
